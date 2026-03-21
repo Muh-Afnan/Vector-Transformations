6851738e58ec5764e3db75f48355d0d79093ba3a
Muh-Afnan <muhammadafnan108@gmail.com>
2026-03-21 22:09:44 +0500

    Implement eigenvalue calculation with matrix validation

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import copy
 from src.matrix import Matrix
+from src.validator import MatrixValidator
 
 
 A = Matrix([[1, 2], [3, 4]])
 
-print(A[0])
+def calculate_eigen_values(matrix:Matrix)->list[int]:
+    shape = matrix.shape()
+    if MatrixValidator.validate_square(shape):
+        counter = 0
+        for row in matrix:
+            matrix[counter][counter].concat("-x")
+            counter +=1
+    matrix        
